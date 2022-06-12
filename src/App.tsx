de15d305eae7f388ae5ab78e4969b48e4c48ec62
Fred Besteman <fred@besteman.net>
2022-06-12 13:33:00 -0400

    feat: table filtering

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import "tachyons";
 import { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
+import { matchSorter } from "match-sorter";
 
 type UserFavorite = {
   id: string;
@@ -37,6 +38,10 @@ const upperLimit = search ? search.split("=")[1] : defaultRecordCount;
 function App() {
   const [userFavorites, setUserFavorites] =
     useState<Array<UserFavorite> | null>(null);
+  const [originalRecords, setOriginalRecords] =
+    useState<Array<UserFavorite> | null>(null);
+
+  const [selectedUser, setSelectedUser] = useState<UserFavorite | null>(null);
 
   useEffect(() => {
     supabase
@@ -45,18 +50,59 @@ function App() {
         "id, name, email, team, animal, musicGenre, song, book, color, movie, drink, food, number, superHero"
       )
       .range(0, Number(upperLimit) - 1)
-      .then(({ data, error }) => setUserFavorites(data));
+      .then(({ data, error }) => {
+        setUserFavorites(data);
+        setOriginalRecords(data);
+      });
   }, []);
+
+  function handleFilter(e: React.ChangeEvent<HTMLInputElement>) {
+    const { value } = e.target;
+    const filteredRecords =
+      value && userFavorites
+        ? matchSorter(userFavorites, value, {
+            keys: Object.keys(userFavorites[0]),
+          })
+        : originalRecords;
+
+    // items.filter(item => item.toLowerCase().includes(inputValue.toLowerCase()))
+
+    setUserFavorites(filteredRecords);
+  }
+
+  function handleSort(colName: string) {
+    const results = originalRecords?.sort((a, b) =>
+      a.animal > b.animal ? 1 : -1
+    );
+    console.log(results);
+    if (results) {
+      setUserFavorites(results);
+    }
+  }
 
   return userFavorites ? (
     <div className="ma1">
-      <h2 className="mv2">Favorite Things: React.js</h2>
-      <h3 className="mv2">record count: {userFavorites.length}</h3>
+      <div className="flex flex-row justify-between">
+        <div>
+          <h2 className="mv2">Favorite Things: React.js</h2>
+          <h3 className="mv2">record count: {userFavorites.length}</h3>
+        </div>
+        <input
+          className="h2"
+          type="text"
+          placeholder="search"
+          onChange={handleFilter}
+        />
+      </div>
       <table className="f6 w-100" cellSpacing="0">
         <thead>
           <tr>
             {Object.keys(userFavorites[0]).map((colName) => (
-              <th className="fw6 tl pa1 pointer bb black--90" key={colName}>
+              <th
+                className="fw6 tl pa1 pointer bb black--90"
+                key={colName}
+                onClick={() => handleSort(colName)}
+              >
                 {colName}
               </th>
             ))}
@@ -64,7 +110,11 @@ function App() {
         </thead>
         <tbody>
           {userFavorites.map((userInfo) => (
-            <tr key={`${userInfo.id}`}>
+            <tr
+              key={`${userInfo.id}`}
+              onClick={() => setSelectedUser(userInfo)}
+              className="pointer dim"
+            >
               {Object.values(userInfo).map((value) => (
                 <td
                   className="pa1 tl bb bl black--90"
